447eb7ec0a3d0b910d6f1f04545b96a45f2dc47d
Arezoo Safari <40689104+safarigha@users.noreply.github.com>
2024-07-16 14:45:01 +0330

    edited commons component to use pages

--- src/components/commons/UI/buttons/BrandColorButton.tsx
+++ src/components/commons/UI/buttons/BrandColorButton.tsx
@@ -4,11 +4,13 @@ const BrandColorButton: React.FC<IBrandColorButton> = ({
   text,
   classNames = "",
   type = "button",
+  disabled = false,
   // ...props
 }) => {
   return (
     <button
       type={type}
+      disabled={disabled}
       className={`items-center gap-2 rounded-md bg-brand-primary hover:bg-brand-primary/90 py-1.5 px-3 text-sm font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[focus]:outline-1 data-[focus]:outline-white ${classNames}`}
       // {...props}
     >
--- src/components/commons/forms/InputForm.tsx
+++ src/components/commons/forms/InputForm.tsx
@@ -1,37 +1,116 @@
-import React from "react";
-import { IInputForm } from "../../../configs/interfaces";
-import Input from "../UI/Input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { ZodSchema } from "zod";
 import BrandColorButton from "../UI/buttons/BrandColorButton";
 
-const InputForm: React.FC<IInputForm> = ({
+interface FormProps {
+  onSubmit: (data: any) => void;
+  fields: { id: string; type: string; label: string }[];
+  submitText: string;
+  schema: ZodSchema<any>;
+  includeCheckbox?: boolean;
+}
+
+const InputForm: React.FC<FormProps> = ({
+  onSubmit,
   fields,
   submitText,
-  errors,
-}: IInputForm) => {
+  schema,
+  includeCheckbox = false,
+}) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(schema),
+  });
+
+  const handleFormSubmit = (data: any) => {
+    onSubmit(data);
+  };
+
   return (
-    <fieldset className="w-full">
+    <form onSubmit={handleSubmit(handleFormSubmit)} className="w-full">
       {fields.map((field) => (
         <div key={field.id}>
-          <Input
-            id={field.id}
+          <label>{field.label}</label>
+          <input
             type={field.type}
-            label={field.label}
-            {...field.register}
-          ></Input>
+            {...register(field.id)}
+            className="mt-2 block w-full h-10 rounded-lg border border-gray-primary px-4 focus:outline-none focus:border-blue-primary"
+          />
           {errors[field.id] && (
-            <p className="text-red-500 text-xs mt-2">
-              {errors[field.id].message}
+            <p className="text-red-500 text-xs mt-2 mb-2">
+              {errors[field.id]?.message as string}
             </p>
           )}
         </div>
       ))}
+      {includeCheckbox && (
+        <div className="flex w-full gap-1 mt-6 items-center">
+          <input
+            type="checkbox"
+            {...register("isCheckedRule")}
+            className="size-4 ml-1 "
+          />
+          <button className="underline font-medium">قوانین و مقررات</button>
+          <div className="font-medium">را می‌پذیرم.</div>
+          {errors.isCheckedRule && (
+            <p className="text-red-500 text-xs ml-2">
+              {errors.isCheckedRule?.message as string}
+            </p>
+          )}
+        </div>
+      )}
       <BrandColorButton
-        type="submit"
         text={submitText}
         classNames="mt-6 w-full h-10"
+        type="submit"
       />
-    </fieldset>
+    </form>
   );
 };
 
 export default InputForm;
+
+// import React from "react";
+// import { IInputForm } from "../../../configs/interfaces";
+// import Input from "../UI/Input";
+// import BrandColorButton from "../UI/buttons/BrandColorButton";
+
+// const InputForm: React.FC<IInputForm> = ({
+//   fields,
+//   submitText,
+//   errors,
+//   disabled,
+//   onSubmit,
+// }: IInputForm) => {
+//   return (
+//     <form onSubmit={onSubmit} className="w-full">
+//       {fields.map((field) => (
+//         <div key={field.id}>
+//           <Input
+//             id={field.id}
+//             type={field.type}
+//             label={field.label}
+//             {...field.register}
+//           ></Input>
+//           {errors[field.id] && (
+//             <p className="text-red-500 text-xs mt-2">
+//               {errors[field.id].message}
+//             </p>
+//           )}
+//         </div>
+//       ))}
+//       <BrandColorButton
+//         type="submit"
+//         text={submitText}
+//         classNames="mt-6 w-full h-10"
+//         disabled={disabled}
+//       />
+//     </form>
+//   );
+// };
+
+// export default InputForm;
