a297b3079c9199312dd3ca4bd1e6a4bf5152553e
Arezoo Safari <40689104+safarigha@users.noreply.github.com>
2024-08-10 10:02:39 +0330

    set all pages for profile section in profile page

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,5 +1,26 @@
+import React, { useState } from "react";
+import Personal from "./Personal";
+import Sidebar from "./Sidebar";
+import Account from "./Account";
+import Settings from "./Setting";
+
 const Profile: React.FC = () => {
-  return <h1>Profile</h1>;
+  const [activeComponent, setActiveComponent] = useState<string>("personal");
+  const handleComponentChange = (type: string) => {
+    setActiveComponent(type);
+  };
+  return (
+    <div className="grid grid-cols-4 h-screen ">
+      <div className="col-span-1 w-[340px]">
+        <Sidebar onComponentChange={handleComponentChange} />
+      </div>
+      <div className="col-span-3 flex justify-center mt-[100px]">
+        {activeComponent === "personal" && <Personal />}
+        {activeComponent === "account" && <Account />}
+        {activeComponent === "setting" && <Settings />}
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
