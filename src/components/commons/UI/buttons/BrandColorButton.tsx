f0f75a5bbeb69dd3bf32d1db7a4aaa00fbd83371
Arezoo Safari <40689104+safarigha@users.noreply.github.com>
2024-08-11 22:40:35 +0330

    updated for onclick in button

--- src/components/commons/UI/buttons/BrandColorButton.tsx
+++ src/components/commons/UI/buttons/BrandColorButton.tsx
@@ -5,11 +5,13 @@ const BrandColorButton: React.FC<IBrandColorButton> = ({
   classNames = "",
   type = "button",
   disabled = false,
+  onClick,
 }) => {
   return (
     <button
       type={type}
       disabled={disabled}
+      onClick={onClick}
       className={`items-center gap-2 rounded-md bg-brand-primary hover:bg-brand-primary/90 py-1.5 px-3 text-sm font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[focus]:outline-1 data-[focus]:outline-white ${classNames}`}
     >
       {text}
